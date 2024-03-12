4cd34291b642ef1795c0f92b0b3f842b41a43b4c
adrien leveille <adrienleveille.contact@gmail.com>
2024-03-12 15:10:51 +0100

    Devise_setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :username, :email, :password, presence: true
+  validates :username, uniqueness: true
+
+  has_many :watches, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
